175f1fc2a93ee1f1be5a0d434a3f07b14fd67720
Marlon Monzon <martianworm17@gmail.com>
2019-06-10 22:04:55 +0800

    Set service as message sender

--- simple-rabbit-app-one/src/main/java/com/simple/web/SimpleRabbitAppOneApplication.java
+++ simple-rabbit-app-one/src/main/java/com/simple/web/SimpleRabbitAppOneApplication.java
@@ -27,28 +27,28 @@ public class SimpleRabbitAppOneApplication {
 		return new TopicExchange(topicExchangeName);
 	}
 	
-	@Bean
-	Binding binding(Queue queue, TopicExchange exchange) {
-		return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
-	}
-	
-	@Bean
-	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-			MessageListenerAdapter listenerAdapter) {
-		
-		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-		
-		container.setConnectionFactory(connectionFactory);
-		container.setQueueNames(queueName);
-		container.setMessageListener(listenerAdapter);
-		
-		return container;
-	}
-	
-	@Bean
-	MessageListenerAdapter listenerAdapter(Receiver receiver) {
-		return new MessageListenerAdapter(receiver, "receiveMessage");
-	}
+//	@Bean
+//	Binding binding(Queue queue, TopicExchange exchange) {
+//		return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+//	}
+//	
+//	@Bean
+//	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+//			MessageListenerAdapter listenerAdapter) {
+//		
+//		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+//		
+//		container.setConnectionFactory(connectionFactory);
+//		container.setQueueNames(queueName);
+//		container.setMessageListener(listenerAdapter);
+//		
+//		return container;
+//	}
+//	
+//	@Bean
+//	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+//		return new MessageListenerAdapter(receiver, "receiveMessage");
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleRabbitAppOneApplication.class, args);
