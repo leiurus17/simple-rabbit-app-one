e2d60fd9581b3a27223bcf43c0da70f4829aed01
Marlon Monzon <martianworm17@gmail.com>
2019-06-10 22:34:10 +0800

    Try sending a message from a Controller

---
+++ simple-rabbit-app-one/src/main/java/com/simple/web/SenderController.java
@@ -0,0 +1,22 @@
+package com.simple.web;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SenderController {
+	
+	private final RabbitTemplate rabbitTemplate;
+	
+	public SenderController(RabbitTemplate rabbitTemplate) {
+		this.rabbitTemplate = rabbitTemplate;
+	}
+	
+	@RequestMapping("send")
+	public void send() {
+		rabbitTemplate.convertAndSend(SimpleRabbitAppOneApplication.topicExchangeName, "foo.bar.baz", "Hello from SenderController!");
+	}
+}
